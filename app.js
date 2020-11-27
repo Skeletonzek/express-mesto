3a072e313e9d93f36765f7df30d64c8a3da3968a
Ilya Zhulanov <zhulanovilya@yandex.ru>
2020-11-28 00:50:33 +0300

    the end

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const { celebrate, Joi, errors } = require('celebrate');
 const bodyParser = require('body-parser');
 const userRouter = require('./routes/users.js');
@@ -22,6 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
+
+app.use(cors());
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
